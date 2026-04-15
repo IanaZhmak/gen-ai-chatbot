190b9ef92df16140b549bbc5053a2e6aa6c6344e
Yana <yazhmak@gmail.com>
2026-04-15 11:57:23 -0400

    add message repository

---
+++ src/main/java/com/gen/ai/chatbot/repository/MessageRepository.java
@@ -0,0 +1,18 @@
+package com.gen.ai.chatbot.repository;
+
+import com.gen.ai.chatbot.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+//    List<Message> findByChatIdOrderByCreatedAtAsc(Long chatId);
+
+    @Query(value = "SELECT * FROM message WHERE chat_id = :chatId ORDER BY created_at ASC", nativeQuery = true)
+    List<Message> findByChatIdOrderByCreatedAtAsc(@Param("chatId") Long chatId);
+
+    @Query(value = "SELECT * FROM chatbot_db.message WHERE chat_id = :chatId ORDER BY id DESC LIMIT 1", nativeQuery = true)
+    Message findByChatIdLastMessage(@Param("chatId") Long chatId);
+}
