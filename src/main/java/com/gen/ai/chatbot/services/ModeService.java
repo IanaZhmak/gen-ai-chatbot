9ca6b4105f618f80e0465fc20d919f742f3927ec
IanaZhmak <yazhmak@gmail.com>
2026-05-04 14:49:21 -0400

    rename to mode service

---
+++ src/main/java/com/gen/ai/chatbot/services/ModeService.java
@@ -0,0 +1,20 @@
+package com.gen.ai.chatbot.services;
+
+import com.gen.ai.chatbot.entity.Mode;
+import com.gen.ai.chatbot.dto.mode.ModeResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ModeService {
+
+    public List<ModeResponseDto> getAllModes() {
+        List<ModeResponseDto> modes = new ArrayList<>();
+        for (Mode mode: Mode.values()) {
+            modes.add(new ModeResponseDto(mode.getKey(), mode.getName()));
+        }
+        return modes;
+    }
+}
