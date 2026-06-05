95042ed94ee9b8c75d8daf8c32a85e661ac53b71
IanaZhmak <yazhmak@gmail.com>
2026-06-05 16:26:11 -0400

    add chat memory config

--- src/main/java/com/gen/ai/chatbot/config/AiConfig.java
+++ src/main/java/com/gen/ai/chatbot/config/AiConfig.java
@@ -1,6 +1,9 @@
 package com.gen.ai.chatbot.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +14,13 @@ public class AiConfig {
     @Bean
     public ChatClient chatClient(ChatModel chatModel) {
         return ChatClient.builder(chatModel).build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(chatMemoryRepository)
+                .maxMessages(10)
+                .build();
     }
 }
